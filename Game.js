424a5bff29e6ebe889c23f44b7063c5482c9e666
AshwinPrasad005 <64143870+AshwinPrasad005@users.noreply.github.com>
2022-01-21 16:47:46 +0530

    Add files via upload

--- Game.js
+++ Game.js
@@ -4,7 +4,7 @@ AFRAME.registerComponent("game-play", {
     },
 
     init: function () {
-        var duration = 120;
+        var duration = 30;
         var timerEl = document.querySelector("#timer");
         this.startTimer(duration, timerEl);
     },
